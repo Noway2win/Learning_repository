7f4d2089cc8b3c6034c0bd37d0898917a922c7c2
Satsukevich Dzmitry <dimamoving@gmail.com>
2021-03-05 23:53:40 +0300

    second practical complete

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "1");
-
+"use strict";
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "1");
+if (numberOfFilms.isNaN || numberOfFilms < 0 || numberOfFilms == null) {
+    alert('Вы лжете');
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "1");
+}
 const personaMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -7,12 +11,30 @@ const personaMovieDB = {
     genres: [],
     private: false,
 };
-console.log(personaMovieDB);
-const filmName1 = prompt("Назовите один из просмотренных фильмов?", "Семь"),
-    filmMark1 = +prompt("Его оценка?", "7"),
-    filmName2 = prompt("Назовите один из просмотренных фильмов?", "Семь"),
-    filmMark2 = +prompt("Его оценка?", "7");
-console.log(filmName1, filmMark1);
-personaMovieDB.movies[`${filmName1}`] = filmMark1;
-personaMovieDB.movies[`${filmName2}`] = filmMark2;
+let filmName,
+    filmMark;
+for (let i = 0; i < 2; i++) {
+    filmName = prompt("Назовите один из просмотренных фильмов?", "Семь");
+    if (filmName.length == 0 || filmName.length >= 50 || filmName == null || filmMark == null) {
+        alert('Введите правду');
+        filmName = prompt("Назовите один из просмотренных фильмов?", "Семь");
+        filmMark = +prompt("Его оценка?", "7");
+    }
+
+    personaMovieDB.movies[`${filmName}`] = filmMark;
+}
+
+const filmCount = personaMovieDB.count;
+
+if (filmCount < 10) {
+    alert(`Просмотренно мало фильмов всего ${filmCount} фильмов`);
+} else if (filmCount < 30) {
+    alert(`Вы обычный зритель, посмотрели уже ${filmCount} фильмов`);
+} else if (filmCount >= 30) {
+    alert(`Вы насмотренный зритель, посмотрели аж ${filmCount} фильмов`);
+} else {
+    alert('Ошибка');
+}
+
+
 console.log(personaMovieDB);
